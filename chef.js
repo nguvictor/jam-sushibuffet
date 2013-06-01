d354de133dc911c5a16c7035934c865c663dd6f5
nguvictor <victor.nguyen@hotmail.com>
2013-06-01 00:52:14 -0400

    Prototyped some functions for chef and fatass

--- chef.js
+++ chef.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
+	var pieceCount = 0; //What piece of sushi assembling
+	
+	function addPiece(){
+	
+	}
+	
+	
+	//Catch the keyboard Events
 	$(document).keydown(function(event) {
 	  if (event.which == 83) {
-		 alert('chef');
+		 
 	   }
 	});
 });
--- fatass.js
+++ fatass.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+
+	//This is the prototype for chef to send a sushi
+	function addSushi(){
+	}
+
+
+	//Catch the keyboard Events
 	$(document).keydown(function(event) {
 	  if (event.which == 76) {
 		 alert('fatass');
