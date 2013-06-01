3e2b7c1f13150d976ee78aafa3f97b66308c597c
nguvictor <victor.nguyen@hotmail.com>
2013-06-01 01:53:56 -0400

    Changed button presses to keyup

--- fatass.js
+++ fatass.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 
 
 	//Catch the keyboard Events
-	$(document).keydown(function(event) {
+	$(document).keyup(function(event) {
 	  if (event.which == 76) {
 		 alert('fatass');
 	   }
