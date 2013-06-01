ee3e78bdcbf1c0b62afdad7b6ad8b0e2612ae2bb
nguvictor <victor.nguyen@hotmail.com>
2013-06-01 00:59:31 -0400

    Improved Hidden Css code and more chef code

--- chef.js
+++ chef.js
@@ -1,15 +1,28 @@
 $(document).ready(function() {
 	var pieceCount = 0; //What piece of sushi assembling
 	
-	function addPiece(){
+	//Add the rice image
+	function addRice(){
+	}
 	
+	//Add the fish
+	function addFish(){
+	}
+	
+	function addPiece(){
+		switch(pieceCount){
+			case 0: addRice();break; //Place the rice
+			case 1: addFish();break; //place the fish
+			case 2: addSushi(); pieceCount = 0;break; //sushi is complete send over the sushi and restart
+			
+		}
 	}
 	
 	
 	//Catch the keyboard Events
 	$(document).keydown(function(event) {
 	  if (event.which == 83) {
-		 
+		 addPiece();
 	   }
 	});
 });
