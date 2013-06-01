caffc32887cbd8d9a884d8f0f5fd2b33b2a9f4dc
nguvictor <victor.nguyen@hotmail.com>
2013-06-01 02:43:42 -0400

    Added dialog, and countdown timers

---
+++ main.js
@@ -0,0 +1,25 @@
+	//Global variables
+	var isRunning = false;
+
+$(document).ready(function() {
+	//Show Dialog press spacee to start
+	
+	//The Ready set go countdown
+	function beginCountDown(){
+	}
+	
+	//The regular countdown timer
+	function countDown(){
+	}
+	//Catch the keyboard Events
+	$(document).keyup(function(event) {
+	  if (event.which == 32) {
+		  //If the game isn't running
+		  if(!isRunning){
+			
+			
+			//Display initial countdown!
+		  }
+	   }
+	});
+});
